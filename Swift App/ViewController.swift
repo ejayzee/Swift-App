43755a03fc85b0d1f57fbb7935298a7668f307de
EMMANUEL J ZAFRA <ejayzee@mac.com>
2016-10-13 20:54:05 -0700

    add numbers in the text fields

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+
+    @IBAction func buttonTapped(_ sender: Any) {
+        theLabel.text = "Answer: \(Int(text1.text!)! + Int(text2.text!)!)"
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
